93f3940d8b72871623b3975ca311995b3adf0e83
Sebastião Teixeira <sebastiao.teixeira@ua.pt>
2022-12-23 23:19:30 +0000

    Fix functions comments

--- database.py
+++ database.py
@@ -10,54 +10,54 @@ try:
 except FileExistsError:
     pass
 
-"""
-Get table file path by
-table name
-"""
 def tablePath(table):
+    """
+    Get table file path by
+    table name
+    """
     return "database/" + table + ".json"
 
-"""
-Verify if a table exists
-"""
 def tableExists(table):
+    """
+    Verify if a table exists
+    """
     try:
         open(tablePath(table), 'r')
         return True
     except FileNotFoundError:
         return False
     
-"""
-Create a new table
-"""
 def createTable(table, public=False):
+    """
+    Create a new table
+    """
     with open(tablePath(table), 'x') as f:
         f.write('{"lastId": 0, "public": ' + ('true' if public else 'false') + ', "tableRows": []}')
     
     return True
     
-"""
-Read table json file
-and loads json
-"""
 def readTable(table):
+    """
+    Read table json file
+    and loads json
+    """
     with open(tablePath(table), 'r') as f:
         data = json.loads(f.read())
 
     return data
 
-"""
-Convert data to json 
-and save table json file
-"""
 def writeTable(table, data):
+    """
+    Convert data to json 
+    and save table json file
+    """
     with open(tablePath(table), 'w') as f:
         f.write(json.dumps(data))
 
-"""
-Add a row to table
-"""
 def addRow(tableName, row):
+    """
+    Add a row to table
+    """
     if not tableExists(tableName):
         createTable(tableName)
 
@@ -75,10 +75,10 @@ def addRow(tableName, row):
 
     return row
 
-"""
-Get tables list
-"""
 def getTables():
+    """
+    Get tables list
+    """
     tables = os.listdir("database")
     for t in tables:
         t = t[:-5]
@@ -90,12 +90,12 @@ def isPublic(tableName):
     data = readTable(tableName)["public"]
     return data
 
-"""
-Get rows from a table
-that satisfies a condition
-ex: getRows("login", lambda row: row["id"] == 7)
-"""
 def getRows(tableName, condition):
+    """
+    Get rows from a table
+    that satisfies a condition
+    ex: getRows("login", lambda row: row["id"] == 7)
+    """
     assert tableExists(tableName), "Table not exists"
     
     data = readTable(tableName)["tableRows"]
@@ -106,32 +106,32 @@ def getRows(tableName, condition):
     
     return result
 
-"""
-Returns True if exists
-at least a row with that
-satisfies a condition
-"""
 def rowExists(tableName, condition):
+    """
+    Returns True if exists
+    at least a row with that
+    satisfies a condition
+    """
     if not len(getRows(tableName, condition)):
         return False
     return True
 
-"""
-Get a row from a table by Id
-"""
 def getRowById(tableName, Id):
+    """
+    Get a row from a table by Id
+    """
     return getRows(tableName, lambda row: row["id"] == Id)[0]
 
-"""
-Get all rows from a table
-"""
 def getAllRows(tableName):
+    """
+    Get all rows from a table
+    """
     return getRows(tableName, lambda row: True)
 
-"""
-Delete a row
-"""
 def removeRow(tableName, row):
+    """
+    Delete a row
+    """
     if not tableExists(tableName):
         createTable(tableName)
 
