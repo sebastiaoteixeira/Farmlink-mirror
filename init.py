55e67afc49847c364fdf53be1c338eb8327cd9d8
Sebastião Teixeira <sebastiao.teixeira@ua.pt>
2022-12-29 21:49:47 +0000

    stock -1 by default --- init.py

--- init.py
+++ init.py
@@ -220,7 +220,7 @@ class MainRequestHandler(server.BaseHTTPRequestHandler):
         producerId = database.getRowById("login", userId)["producerId"]
         if not database.tableExists("products"):
             database.createTable("products", True)
-        product = database.addRow("products", {"name": self.fields.get("name"), "type": self.fields.get("type"), "price": self.fields.get("price"), "stock": self.fields.get("stock"), "description": self.fields.get("description"), "img": self.fields.get("img"), "visible": self.fields.get("visible"), "producerId": producerId})
+        product = database.addRow("products", {"name": self.fields.get("name"), "type": self.fields.get("type"), "price": self.fields.get("price"), "stock": (-1 if self.fields.get("stock") is None else self.fields.get("stock")), "description": self.fields.get("description"), "img": self.fields.get("img"), "visible": True, "producerId": producerId})
         self.send_response(200)
         self.send_header('Content-type', "application/json")
         self.end_headers()
